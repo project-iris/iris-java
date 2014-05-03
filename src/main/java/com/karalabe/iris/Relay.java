0183eecda95ffb4cf31f90111f488e14f76c4d17
Péter Szilágyi <peterke@gmail.com>
2014-05-03 14:20:41 +0300

    Relay constructor.

--- src/main/java/com/karalabe/iris/Relay.java
+++ src/main/java/com/karalabe/iris/Relay.java
@@ -2,14 +2,19 @@ package com.karalabe.iris;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.List;
 
 /*
+* Message relay between the local app and the local iris node.
 **/
-
 public class Relay {
+    // Network layer fields
+    private Socket socket; // Network connection to the iris node
 
-    @NotNull public static List<Integer> run() {
-        return null;
+    public Relay(int port, String cluster) throws IOException {
+        socket = new Socket(InetAddress.getLoopbackAddress(), port);
     }
 }
