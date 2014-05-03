858fef7188779f8c744075c3f7721e972146bc9c
Pap Lőrinc <paplorinc@yahoo.com>
2014-05-03 13:09:28 +0300

    Initial structural commit;

---
+++ src/main/java/com/karalabe/iris/Binding.java
@@ -0,0 +1,15 @@
+package com.karalabe.iris;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/*
+**/
+
+public class Binding {
+
+    @NotNull public static List<Integer> run() {
+        return null;
+    }
+}
---
+++ src/test/java/com/karalabe/iris/BindingTest.groovy
@@ -0,0 +1,8 @@
+package com.karalabe.iris
+
+import org.junit.Test
+
+class BindingTest {
+
+    @Test public void testSimpleCases() throws Exception {}
+}
