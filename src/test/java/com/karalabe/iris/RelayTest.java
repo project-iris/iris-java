48dc6a76635368620b5231751f83bc4d1463c5b1
Pap Lőrinc <paplorinc@yahoo.com>
2014-05-03 13:45:24 +0300

    Test correction;

--- src/test/java/com/karalabe/iris/RelayTest.java
+++ src/test/java/com/karalabe/iris/RelayTest.java
@@ -4,16 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class RelayTest {
 
     @Test public void testSimpleCases() throws Exception {
-        try {
-            final ServerSocket socket = new ServerSocket(55555);
+        try (final Socket socket = new Socket(InetAddress.getLocalHost(), 55555)) {
+            socket.isClosed();
         }
         catch (IOException e) {
-            Assert.fail();
+            Assert.fail(e.getMessage());
         }
     }
 }
