30014fcfb18cfc260a0b088a7a573c3e816ed61d
Péter Szilágyi <peterke@gmail.com>
2014-06-02 01:46:01 +0300

    Comment out until further refactor.

--- src/main/java/com/karalabe/iris/protocol/publish_subscribe/SubscribeTransfer.java
+++ src/main/java/com/karalabe/iris/protocol/publish_subscribe/SubscribeTransfer.java
@@ -1,10 +1,9 @@
 package com.karalabe.iris.protocol.publish_subscribe;
 
-import com.karalabe.iris.protocol.OpCode;
-import com.karalabe.iris.protocol.ProtocolBase;
 import com.karalabe.iris.TopicHandler;
 import com.karalabe.iris.TopicLimits;
 import com.karalabe.iris.callback.CallbackHandlerRegistry;
+import com.karalabe.iris.protocol.ProtocolBase;
 import com.karalabe.iris.protocol.TransferBase;
 import com.karalabe.iris.protocol.Validators;
 import org.jetbrains.annotations.NotNull;
@@ -16,21 +15,21 @@ public class SubscribeTransfer extends TransferBase implements SubscribeApi {
 
     @Override public void subscribe(@NotNull final String topic, @NotNull final TopicHandler handler, final TopicLimits limits) throws IOException {
         Validators.validateTopic(topic);
-
+/*
         final Long subscriptionId = addCallbackHandler(handler);// TODO is the topic the id?
         protocol.send(OpCode.SUBSCRIBE, () -> {
             protocol.sendVarint(subscriptionId); // TODO ???
             protocol.sendString(topic);
-        });
+        });*/
     }
 
     @Override public void unsubscribe(@NotNull final String topic, @NotNull final TopicHandler handler) throws IOException {
         Validators.validateTopic(topic);
-
+/*
         final Long subscriptionId = addCallbackHandler(handler);// TODO is the topic the id?
         protocol.send(OpCode.UNSUBSCRIBE, () -> {
             protocol.sendVarint(subscriptionId); // TODO
             protocol.sendString(topic);
-        });
+        });*/
     }
 }
